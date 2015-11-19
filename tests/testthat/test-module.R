fecbde1b6a12ef2732197b1f8aeba644d78cdd48
RobertZK <technoguyrob@gmail.com>
2015-11-18 19:07:17 -0800

    Check that failure cases for module constructor work as expected.

--- tests/testthat/test-module.R
+++ tests/testthat/test-module.R
@@ -2,7 +2,10 @@ context("module")
 
 describe("Failure cases", {
   test_that("it fails to construct a module if a character is not passed as the first argument", {
-    expect_error(module(NULL), "Invalid path passed")
+    lapply(list(NULL, new.env(), list(a = 1), 1, function() { }, 1.0 + 2.0i), function(el) {
+      expect_error(module(el), "Invalid path passed")
+      expect_error(module(el), class(el)[1L])
+    })
   })
 })
 
